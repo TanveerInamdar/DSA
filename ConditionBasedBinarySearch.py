f85c63bc335aa7c5ad700c8441524b8fd8294d74
Tanveer Inamdar <tinamdar@ttu.edu>
2025-08-17 19:49:13 -0500

    Binary Search - Conditional

---
+++ ConditionBasedBinarySearch.py
@@ -0,0 +1,21 @@
+B = [False, False, True, True, True, True]
+
+def BinarySearch(arr):
+    N = len(arr)
+
+    L = 0
+    R = N-1
+
+    while L < R:
+        mid = (L + R) // 2
+
+        if arr[mid]:
+            R = mid
+        else:
+            L = mid + 1
+
+
+    return L
+
+
+print(BinarySearch(B))
